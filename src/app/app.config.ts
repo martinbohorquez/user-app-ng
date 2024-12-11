b25f693fed02ec5e46b7e1329ba4956f460b4f6a
Martin Bohorquez <121200468+martinbohorquez@users.noreply.github.com>
2024-12-10 22:14:26 -0500

    add effects and store-devtools to dependencies, update appConfig: provideEffects and provideStoreDevtools

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,13 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { routes } from './app.routes';
 import { tokenInterceptor } from './interceptors/token.interceptor';
+import { UserEffects } from './store/user.effects';
 import { usersReducer } from './store/users.reducer';
 
 export const appConfig: ApplicationConfig = {
@@ -14,6 +17,8 @@ export const appConfig: ApplicationConfig = {
 		provideHttpClient(withInterceptors([tokenInterceptor])),
 		provideStore({
 			users: usersReducer
-		})
+		}),
+		provideEffects(UserEffects),
+		provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
 	]
 };
